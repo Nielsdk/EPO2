937238534baffcb0d35f142f91d413c3ad5d7c0b
Nielsdk <Niels.dk@hotmail.com>
2016-05-29 10:28:14 +0200

    Ik probeer dit ook maar
    Ik weet niet wat ik aan het doen ben :(

--- mazerouter2/rx_translator.vhdl
+++ mazerouter2/rx_translator.vhdl
@@ -9,7 +9,7 @@ entity rx_translator is
 		translator_out_reset : in std_logic;
 		flag_reset	: out std_logic; -- als het nieuwe signaal gelezen is, stel de flag op '0'.
 
-		translator_out	: out std_logic_vector(3 downto 0); -- output, vertaald volgens bovenstaand protocol
+		translator_out	: out std_logic_vector(7 downto 0); -- output, vertaald volgens bovenstaand protocol
 
 		receiver_check	: out std_logic_vector(7 downto 0); -- Stuur de gelezen input naar de transmitter
 		send_check	: out std_logic -- Zorg dat die daadwerkelijk stuurt!
@@ -19,12 +19,19 @@ end entity rx_translator;
 architecture bh of rx_translator is
 	type state_type is (	idle, -- Wachten op signaal
 				check, -- Controle of het wel goed is aangekomen
-				forward, backward, stop,
-				right90, right, fastright,
-				left90, left, fastleft,
-				reset_output
+				stop, 			-- 10000000
+				forward, 		-- 10000001
+				right,			-- 10000010
+				left,			-- 10000100
+				backward,		-- 10001000
+				station_forward, 	-- 11000001
+				station_right,		-- 11000010
+				station_left,		-- 11000100
+				station_backward,	-- 11001000
+				reset_output -- Breng hem automatisch in 00000000
+			--29/5/2016: Handshake is nu naar  11111111
 			);
-	signal translator_reg, translator_next : std_logic_vector(3 downto 0);
+	signal translator_reg, translator_next : std_logic_vector(7 downto 0);
 	signal receiver_check_reg, receiver_check_next : std_logic_vector(7 downto 0);
 	signal receiver_in_reg, receiver_in_next : std_logic_vector(7 downto 0);
 	signal state_reg, state_next : state_type;
@@ -70,17 +77,17 @@ begin
 				end if;
 			when check =>
 				if(flag_in = '1' or HANDSHAKE = 0) then
-					if(receiver_in = "10000000" or HANDSHAKE = 0) then
+					if(receiver_in = "11111111" or HANDSHAKE = 0) then
 						case receiver_in_reg is
-							when "00000001" => state_next <= forward;
-							when "00000010" => state_next <= right;
-							when "00000011" => state_next <= fastright;
-							when "00000100" => state_next <= right90;
-							when "00000101" => state_next <= left;
-							when "00000110" => state_next <= fastleft;
-							when "00000111" => state_next <= left90;
-							when "00001000" => state_next <= backward;
-							when "00000000" => state_next <= stop;
+							when "10000001" => state_next <= forward;
+							when "10000010" => state_next <= right;
+							when "10000100" => state_next <= left;
+							when "10001000" => state_next <= backward;
+							when "11000001" => state_next <= station_forward;
+							when "11000010" => state_next <= station_right;
+							when "11000100" => state_next <= station_left;
+							when "11001000" => state_next <= station_backward;
+							when "10000000" => state_next <= stop;
 							when others => state_next <= idle;
 						end case;
 					else
@@ -88,34 +95,37 @@ begin
 					end if;
 				end if;
 			when forward =>
-				translator_next <= "0001";
+				translator_next <= "10000001";
 				state_next <= idle;
 			when right =>
-				translator_next <= "0010";
-				state_next <= idle;
-			when fastright =>
-				translator_next <= "0011";
-				state_next <= idle;
-			when right90 =>
-				translator_next <= "0100";
+				translator_next <= "10000010";
 				state_next <= idle;
 			when left =>
-				translator_next <= "0101";
-				state_next <= idle;
-			when fastleft =>
-				translator_next <= "0110";
-				state_next <= idle;
-			when left90=>
-				translator_next <= "0111";
+				translator_next <= "10000100";
 				state_next <= idle;
 			when backward =>
-				translator_next <= "1000";
+				translator_next <= "10001000";
+				state_next <= idle;
+			when station_forward =>
+				translator_next <= "11000001";
+				state_next <= idle;
+			when station_right =>
+				translator_next <= "11000010";
+				state_next <= idle;
+			when station_left =>
+				translator_next <= "11000100";
+				state_next <= idle;
+			when station_backward =>
+				translator_next <= "11001000";
+				state_next <= idle;
+			when stop =>
+				translator_next <= "10000000";
 				state_next <= idle;
 			when reset_output =>
-				translator_next <= "0000";
+				translator_next <= "00000000";
 				state_next <= idle; 
 			when others =>
-				translator_next <= "0000";
+				translator_next <= "00000000";
 				state_next <= idle; 
 		end case;
 	end process;
@@ -127,4 +137,6 @@ end architecture bh;
 
 	
 	
+
+
 
