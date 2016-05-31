e7de7d00f1da21db3c46a6e5d65a3b95387eaeea
jbee2 <jor.bentvelsen@gmail.com>
2016-05-31 22:44:59 +0200

    Add files via upload

--- mazerouter2/override_controller_2.vhdl
+++ mazerouter2/override_controller_2.vhdl
@@ -206,29 +206,66 @@ begin
 		when left_station => -- VOORBEELD VOOR FORWARD_STATION COMMANDO.
 			
 
-			if ((sensor_l = '0' or sensor_m = '0' or sensor_r = '0') and station_state = first) then -- lijnvolgen zolang in ieder geval 1 van de sensoren een lijn ziet. Zo rijd hij tot het einde van de lijn (waar ze alle drie wit (='1') worden)
-				pwm_count_reset <= '0';
-				override <= '0';
-				override_vector <= "1000"; -- moet iets zijn
-				override_cont_new_state <= left_station;
-				translator_out_reset <= '0';
-				new_station_state <= first;
-			elsif(sensor_l = '1') then -- Bocht maken als hij aan het einde van de lijn is (vorige conditie eindigt). Dan geldt: sensor_l ='1' (wit).  Dan  180 graden RECHTSOM draaien. De linker sensor zal als laatste weer zwart worden. Dan verder naar de volgende stap.
-				pwm_count_reset <= '0';
-				override <= '1';
-				override_vector <= "0100"; -- drive_motor_fastright.
-				override_cont_new_state <= left_station;
-				translator_out_reset <= '0';
-				new_station_state <= second;			
-			else
-				long_pwm_count_reset <= '0'; -- De robot is klaar om weer lijn te volgen, hij verlaat de override stand. Hij moet hier weer een signaal sturen naar de pc dat hij het volgende commando wil.
-				pwm_count_reset <= '1';
-				override <= '0';
-				override_vector <= "0000";-- moet iets zijn
-				override_cont_new_state <= read_sensor_and_listen;
-				translator_out_reset <= '1';
-				new_station_state <= third;
-			end if;
+--			if ((sensor_l = '0' or sensor_m = '0' or sensor_r = '0') and station_state = first) then -- lijnvolgen zolang in ieder geval 1 van de sensoren een lijn ziet. Zo rijd hij tot het einde van de lijn (waar ze alle drie wit (='1') worden)
+--				pwm_count_reset <= '0';
+--				override <= '0';
+--				override_vector <= "1000"; -- moet iets zijn
+--				override_cont_new_state <= left_station;
+--				translator_out_reset <= '0';
+--				new_station_state <= first;
+--			elsif(sensor_l = '1') then -- Bocht maken als hij aan het einde van de lijn is (vorige conditie eindigt). Dan geldt: sensor_l ='1' (wit).  Dan  180 graden RECHTSOM draaien. De linker sensor zal als laatste weer zwart worden. Dan verder naar de volgende stap.
+--				pwm_count_reset <= '0';
+--				override <= '1';
+--				override_vector <= "0100"; -- drive_motor_fastright.
+--				override_cont_new_state <= left_station;
+--				translator_out_reset <= '0';
+--				new_station_state <= second;			
+--			else
+--				long_pwm_count_reset <= '0'; -- De robot is klaar om weer lijn te volgen, hij verlaat de override stand. Hij moet hier weer een signaal sturen naar de pc dat hij het volgende commando wil.
+--				pwm_count_reset <= '1';
+--				override <= '0';
+--				override_vector <= "0000";-- moet iets zijn
+--				override_cont_new_state <= read_sensor_and_listen;
+--				translator_out_reset <= '1';
+--				new_station_state <= third;
+--			end if;
+
+			pwm_count_reset <= '0';
+			override_vector <= "1000"; -- moet iets zijn
+			translator_out_reset <= '0';
+			override_cont_new_state <= left_station;
+			
+			case station_state is
+			
+				when first => 	-- Lijnvolgen
+					override <= '0';
+							
+					if(sensor_l = '0' or sensor_m = '0' or sensor_r = '0') then --lijnvolgen zolang in ieder geval 1 van de sensoren een lijn ziet. Zo rijd hij tot het einde van de lijn (waar ze alle drie wit (='1') worden)
+						new_station_state <= first;
+					else
+						new_station_state <= second;
+					end if;
+					
+				when second => --Bocht maken als hij aan het einde van de lijn is. Dan geldt: sensor_l ='1' (wit).  Dan  180 graden RECHTSOM draaien. De linker sensor zal als laatste weer zwart worden. Dan verder naar de volgende stap.
+					
+					override <= '1';
+					override_vector <= "0100"; -- drive_motor_fastright.
+					
+					if(sensor_l = '1') then
+						new_station_state <= second;
+					else
+						new_station_state <= third;
+					end if;
+				
+				when third =>
+					long_pwm_count_reset <= '0'; -- De robot is klaar om weer lijn te volgen, hij verlaat de override stand. Hij moet hier weer een signaal sturen naar de pc dat hij het volgende commando wil.
+					pwm_count_reset <= '1';
+					override <= '0';
+					override_cont_new_state <= read_sensor_and_listen;
+					translator_out_reset <= '1';
+					new_station_state <= first;
+				end case;
+					
 
 		when right_station => 
 		
